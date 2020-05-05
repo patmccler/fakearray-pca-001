1b04b1b6a787fbcbc200703e8a06c0484cc658a4
Pat McClernan <patmccler@gmail.com>
2020-05-05 14:19:40 -0400

    tests passing

--- lib/fake_array.rb
+++ lib/fake_array.rb
@@ -1,7 +1,24 @@
 class FakeArray
+  def initialize (*args)
+    @my_values = {}
+    args.each_with_index do |arg, i|
+      @my_values[i] = arg
+    end
+  end
 
-  # you'll need a splat in this class somewhere
+  def [](index)
+    @my_values[index]
+  end
 
+  def each
+    count = 0
+    while count < @my_values.keys.length
+      yield @my_values[count]
+      count += 1
+    end
+  end
 
-
+  def first
+    @my_values[0]
+  end
 end
